32bdb6df63b29f9f754e5252ff188b3983b2dc15
z-gonzales <zoegonzales03@gmail.com>
2019-02-09 15:13:42 -0700

    code for seatgeek ajax call

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,27 @@
 
 
-// Code for using Eventful API
+// Code for using SeatGeek API
 var city = '';
 var keyword = '';
-var queryURLEventful = '';
-var date = '';
+var queryURL = '';
 
 function renderEvents(e) {
     e.preventDefault();
     city = $("#city-name").val().trim();
     keyword = $("#keyword").val().trim();
-    date = $('#date').val();
-    var apiKey = 'TnjCPgZgrN3KH5Vb';
-    queryURLEventful = `https://api.eventful.com/json/events/search?app_key=${apiKey}&location=${city}&date=Today`;
+    var apiKey = 'MTUyMzkxNzF8MTU0OTc0OTQ1NC4wNA';
+    queryURL = `https://api.seatgeek.com/2/events?client_id=${apiKey}&venue.city=${city}`;
 
-    // Conditionals allowing for certain input to be optional
-    if (keyword) {
-        queryURLEventful = `https://api.eventful.com/json/events/search?app_key=${apiKey}&keywords=${keyword}&location=${city}&date=Future`;
+    if(keyword) {
+        queryURL = `https://api.seatgeek.com/2/events?client_id=${apiKey}&venue.city=${city}&q=${keyword}`;
     }
 
-    if (date !== "Today") {
-        queryURLEventful = `https://api.eventful.com/json/events/search?app_key=${apiKey}&location=${city}&date=${date}`;
-    }
-
-    if (date !== "Today" && keyword) {
-        queryURLEventful = `https://api.eventful.com/json/events/search?app_key=${apiKey}&location=${city}&date=${date}`;
-    }
-
-    // Call to Eventful API
+    // Call to SeatGeek API
     $.ajax({
-        // ***Lines 32 - 34 are an attempt at avoiding CORS within the call - need assistance in class***
-        // beforeSend: function(request) {
-        // request.setRequestHeader('Access-Control-Allow-Origin', '*');
-        // },
-        url: queryURLEventful,
-        method: "GET"
-    }).then(function(response){
-        response = JSON.parse(response);
-        // Response returns an object, within object is all events in an array (10 at a time, though I believe you can add more)
-        console.log(response);
-        for (var i=0; i < response.events.event.length; i++) {
-            // Check titles of events
-            console.log(response.events.event[i].title);
-            // div to hold each new event
-            var newEvent = $("<div>");
-            // Event Title
-            var title = $("<p>");
-            title.html(`Event Title: ${response.events.event[i].title}`);
-            // Event Location - city, state, zip
-            // **Note on Location - would need to add conditional for zip code since sometimes it is null
-            var location = $("<p>");
-            location.html(`Location: ${response.events.event[i].city_name}, ${response.events.event[i].region_abbr}, ${response.events.event[i].postal_code}`);
-            // Event Time - needs to be converted (maybe with Moment.js)
-            // **Note on time - sometimes returns past events when searching for events marked "Today"
-            var time = $("<p>");
-            time.html(`Time and date: ${response.events.event[i].start_time}`);
-            // External link leads to the event posting on Eventful's website
-            var link = $("<a>");
-            link.attr("href", response.events.event[i].url);
-            link.text("See Event");
-            link.attr("target", "_blank");
-            // Following variables create new form (currently inactive)
-            var newInput = $("<label>");
-            var distContent = $("<input>");
-            var distBtn = $("<input>");
-            newInput.attr("for", "distance");
-            distContent.attr("type", "text");
-            distContent.attr("id", "distance");
-            distBtn.attr("type", "submit");
-            distBtn.attr("id", "get-restaurants");
-            // Text for new form
-            // the idea here is that users will enter a number to represent miles
-            // this will be added to an API call for restaurants in the area
-            // tentative depending on what we want 2nd API to do
-            newInput.html(` Find restaurants within `);
-            newInput.append(distContent, ` miles of this event. `, distBtn);
-            newEvent.append(title, location, time, link, newInput);
-            // Appends all to empty div
-            $("#events").append(newEvent);
-        }
+        url: queryURL,
+        method: 'GET'
+    }).then(function(data){
+        console.log(data);
     });
 }
 
